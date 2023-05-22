ae21544a87355701954d71b92619dbc389db18f7
ZeyadMoustafa <zeyad20062018@gmail.com>
2023-05-22 10:26:08 +0300

    Added api documenation and cors headers

--- blog/urls.py
+++ blog/urls.py
@@ -19,13 +19,12 @@ schema_view = get_schema_view(
     permission_classes=(permissions.AllowAny,),
 )
 urlpatterns = [
-    path('', include('core.urls')),
     path('accounts/', include('accounts.urls')),
     path('posts/', include('posts.urls')),
     path('admin/', admin.site.urls),
     path('blogs/',include('blogs.urls')),
     path('__debug__/',include('debug_toolbar.urls')),
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 
 ] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
